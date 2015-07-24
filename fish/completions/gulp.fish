2a112e27d164785211180b77d360f592efeca0b1
seventyeight <phitau78@gmail.com>
2015-07-24 15:46:44 +0200

    add fish completions

---
+++ fish/completions/gulp.fish
@@ -0,0 +1,7 @@
+#
+# To enable fish <tab> completion for gulp, add the following line to
+# your ~/.config/fish/config.fish file:
+#
+# gulp --completion=fish | source
+
+complete -c gulp -a "(gulp --tasks-simple)" -f
